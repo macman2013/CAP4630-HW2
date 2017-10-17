5f675f568dd51478ccb5a89716e15df3384e65d2
Krista Shuckerow <kshuckerow@gmail.com>
2017-10-17 14:49:01 -0400

    Added second hint

--- CSP-try.py
+++ CSP-try.py
@@ -13,7 +13,18 @@ def hint1(student, distance):
         return
     return student, distance
 
+def hint2(student, color):
+    if(student == "Henrietta" and color != "silver"):
+        return
+    if(student != "Henrietta" and color == "silver"):
+        return
+    return student, color
+
+
 problem.addConstraint(FunctionConstraint(hint1), ["students", "distances"])
-#Check that constraints are working
+#Check that hint 1 constraints are working
 print len(problem.getSolutions())
-print(problem.getSolutions())
+
+#check that hint2 constraints are working
+problem.addConstraint(FunctionConstraint(hint2), ["students", "colors"])
+print len(problem.getSolutions())
