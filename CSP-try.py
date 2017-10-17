5716fd6fd87515ba24d816968efa3b3649ab86ee
Krista Shuckerow <kshuckerow@gmail.com>
2017-10-17 14:59:44 -0400

    Started hint 3 and added reference to api in Readme

--- CSP-try.py
+++ CSP-try.py
@@ -5,14 +5,18 @@ problem.addVariable("colors", ["black", "blue", "pink", "silver"])
 problem.addVariable("distances", [15, 25, 35, 45])
 print len(problem.getSolutions())
 
-
+'''
+Henrietta's design went 35 feet
+'''
 def hint1(student, distance):
     if(student == "Henrietta" and distance != 35):
         return
     if(student != "Henrietta" and distance == 35):
         return
     return student, distance
-
+'''
+Henrietta's deisgn was silver
+'''
 def hint2(student, color):
     if(student == "Henrietta" and color != "silver"):
         return
@@ -20,6 +24,11 @@ def hint2(student, color):
         return
     return student, color
 
+'''
+Omar's design went somewhat farther than the silver airplane.
+'''
+def hint3(student, color, distance):
+    if(student == "Omar")
 
 problem.addConstraint(FunctionConstraint(hint1), ["students", "distances"])
 #Check that hint 1 constraints are working
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CAP4630-HW2
-Version control for my AI HW. 
+Version control for my AI HW.
+API info for the CSP problems
+http://labix.org/doc/constraint/
