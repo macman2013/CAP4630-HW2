7a5ff5d55f66554783350fd73804fb30992969e9
Krista Shuckerow <kshuckerow@gmail.com>
2017-10-17 14:45:23 -0400

    added first constraint

--- CSP-try.py
+++ CSP-try.py
@@ -2,4 +2,18 @@ from constraint import *
 problem = Problem()
 problem.addVariable("students", ["Ella", "Henrietta", "Omar", "Valerie"])
 problem.addVariable("colors", ["black", "blue", "pink", "silver"])
-problem.addVariable("disances", [15, 25, 35, 45])
+problem.addVariable("distances", [15, 25, 35, 45])
+print len(problem.getSolutions())
+
+
+def hint1(student, distance):
+    if(student == "Henrietta" and distance != 35):
+        return
+    if(student != "Henrietta" and distance == 35):
+        return
+    return student, distance
+
+problem.addConstraint(FunctionConstraint(hint1), ["students", "distances"])
+#Check that constraints are working
+print len(problem.getSolutions())
+print(problem.getSolutions())
