9c50a865b5a86e2cecc19ca247273d5e273701ed
Krista Shuckerow <kshuckerow@gmail.com>
2017-10-17 14:19:10 -0400

    set up problem

---
+++ CSP-try.py
@@ -0,0 +1,5 @@
+from constraint import *
+problem = Problem()
+problem.addVariable("students", ["Ella", "Henrietta", "Omar", "Valerie"])
+problem.addVariable("colors", ["black", "blue", "pink", "silver"])
+problem.addVariable("disances", [15, 25, 35, 45])
