c4148944bb856798c7a5ce4e40bb7636cb8c61ba
Dale Keith <dkeit008@fiu.edu>
2017-10-20 03:18:43 -0400

    Finished Hints 1, 2, 4, 6, and 7 of Extra Credit CSP

--- CSP-large-ec.py
+++ CSP-large-ec.py
@@ -1,94 +1,93 @@
 from constraint import *
 '''
-The Daily Ray is either the vessel that went to Rainbow Reef or Captain Romero's Vessel
+FINISHED
+The car with the YGA-441 plates was fined 25 dollars more than the vehicle from Hawaii
 '''
-def hint1(boat, location, captain):
-    if(boat == "Daily Ray" and not ((location == "Rainbow Reef") != (captain == "Romero" ))):
-        return False
-    return True
-'''
-The vessel that went to Rainbow Reef saw fewer manatees than the Watery Pete
-'''
-def hint2(boat, location, manatee, WPboat_manatees):
-    if(location == "Rainbow Reef"):
-        #The watery pete did not go to rainbow reef
-        if(boat == "Watery Pete"):
+def hint1(fine1, fine2, state, license_plate):
+    if(license_plate == "YGA-441" and state == "Hawaii"):
+        if(fine1 - fine2 != 25):
             return False
-        #we Know WP is #3
-        if(manatee >= WPboat_manatees):
+    elif(state == "Hawaii" and license_plate == "YGA-441"):
+        if(fine2 - fine1 != 25):
             return False
     return True
 
 '''
-The Boat that went to Rainbow Reef, Captain Yang's boat, and the Samantha are three different boats
+FINISHED
+The Fierro was fined somewhat less than the automobile with GGZ-007
 '''
-def hint3(boat, location, captain):
+def hint2(car, license_plate, fine):
+    #The Fierro is not the car with license GGZ-007
+    if(car == "Fierro" and license_plate == "GGZ-007"):
+        return False
+    #GGZ-007 cannot be lowest fined
+    if(license_plate == "GGZ-007" and fine == 25):
+        return False
+    #The Fierro is not the highest fined
+    if(car == "Fierro" and fine == 100):
+        return False
+    return True
+
+'''
+The car that went to Rainbow Reef, license_plate Yang's car, and the Samantha are three different cars
+'''
+def hint3(car, state, license_plate):
     #They are all mutually exlusive
-    if(boat == "Samantha" and (location == "Rainbow Reef" or captain == "Yang")):
+    if(car == "Samantha" and (state == "Rainbow Reef" or license_plate == "Yang")):
         return False
-    if(location == "Rainbow Reef" and (boat == "Samantha" or captain == "Yang")):
+    if(state == "Rainbow Reef" and (car == "Samantha" or license_plate == "Yang")):
         return False
-    if(captain == "Yang" and (boat == "Samantha" or location == "Rainbow Reef")):
+    if(license_plate == "Yang" and (car == "Samantha" or state == "Rainbow Reef")):
         return False
     return True
 
 
 '''
-The vessel that went to Betty Beach saw 2 more manatees than the boat that went to Rainbow Reef
+FINSHED
+The vehicle that received the $50 fine, the Grandero, and the Injitsu are three different cars
 '''
-def hint4(location1, location2, manatee1, manatee2):
-    if(location1 == "Betty Beach" and location2 == "Rainbow Reef"):
-        if(manatee1 - manatee2 != 2):
-            return False
-    elif(location1 == "Rainbow Reef" and location2 == "Betty Beach"):
-        if(manatee2 - manatee1 != 2):
-            return False
+def hint4(car, fine, car2):
+    #They are all mutually exlusive
+    if(car == "Injitsu" and (fine == 50 or car2 == "Grandero")):
+        return False
+    if(fine == 50 and (car == "Injitsu" or car2 == "Grandero")):
+        return False
+    if(car2 == "Grandero" and (car == "Injitsu" or fine == 50)):
+        return False
     return True
 
 '''
-The vessel that saw 5 manatees didn't go to Arno's Spit
+The vessel that saw 5 fines didn't go to Arno's Spit
 '''
-def hint5(location, manatee):
+def hint5(state, fine):
     #Ella's plane when 10 feet further than black plane so Ella's is pink
-    if(manatee == 5 and location == "Arno's Spit"):
+    if(fine == 5 and state == "Arno's Spit"):
         return False
     return True
 
 '''
-The boat that saw 3 manatees is either Captain Yang's boat or the Samantha
+FINISHED
+The Grandero was fined somewhat more than the car with the FRZ-192 plates
 '''
-def hint6(boat, manatee, captain):
-     if(manatee == 3):
-         #One or the other
-         if((captain == "Yang") != (boat == "Samantha")):
-             return True
-         return False
-     return True
-
-'''
-Of the Foxy Roxy and the vessel that went to Betty Beach, one saw 3 manatees and the other was lead by Captain Armstrong
-'''
-def hint7(boat, location, manatee, captain, FR, manateeFR, CapFR):
-    #Foxy Roxy did not go to betty Beach
-    if(boat == "Foxy Roxy" and location == "Betty Beach"):
+def hint6(car, fine, license_plate):
+    #Grandero does not have the FRZ-192 plates
+    if(car == "Grandero" and license_plate == "FRZ-192"):
         return False
-    #Foxy Roxy has to have at least one of these true in set
-    if(not(manateeFR == 3 or CapFR == "Armstrong")):
+    #FRZ-192 plates cannot be fined the most
+    if(license_plate == "FRZ-192" and fine == 100):
         return False
-    if(location == "Betty Beach"):
-        #at least one has to be true
-        #since we already passed Foxy Roxy condition and answers are unique just check if it has one
-        if(not(manatee == 3 or captain == "Armstrong")):
-            return False
+    #Grandero cannot be fined the least
+    if(car == "Grandero" and fine == 25):
+        return False
     return True
 
 '''
-The Samantha went to Betty Beach
+FINISHED
+The vehicle that received the $100 fine doesn't have the GGZ-007 plates
 '''
-def hint8(boat, location):
-    if(boat == "Samantha" and location != "Betty Beach"):
-        return False
-    if(boat != "Samantha" and location == "Betty Beach"):
+def hint7(license_plate, fine):
+    # $100 fine does not have the GGZ-007 plates
+    if(fine == 100 and license_plate == "GGZ-007"):
         return False
     return True
 
@@ -96,54 +95,53 @@ def hint8(boat, location):
 Prints final set of answers for the CSP
 '''
 def answer_set(x):
-    Daily_Ray = []
-    Foxy_Roxy = []
-    Samantha = []
-    Watery_Pete = []
-    answer_list = [Daily_Ray, Foxy_Roxy, Samantha, Watery_Pete]
+    Cavalo = []
+    Fierro = []
+    Grandero = []
+    Injitsu = []
+    answer_list = [Cavalo, Fierro, Grandero, Injitsu]
     for a in x:
         if a[-1] == "0":
-            Daily_Ray.append(x[a])
+            Cavalo.append(x[a])
         if a[-1] == "1":
-            Foxy_Roxy.append(x[a])
+            Fierro.append(x[a])
         if a[-1] == "2":
-            Samantha.append(x[a])
+            Grandero.append(x[a])
         if a[-1] in "3":
-            Watery_Pete.append(x[a])
-    for boat in answer_list:
-        print boat
+            Injitsu.append(x[a])
+    for car in answer_list:
+        print car
 
 def main():
 
     problem = Problem()
-    boats = ["Daily Ray", "Foxy Roxy", "Samantha", "Watery Pete"]
-    captains = ["Armstrong", "Jacobson", "Romero", "Yang"]
-    locations = ["Arno's Spit", "Betty Beach", "Rainbow Reef", "Trey's Tunnel"]
-    manatees = [3, 4, 5 ,6]
+    cars = ["Cavalo", "Fierro", "Grandero", "Injitsu"]
+    license_plates = ["FRZ-192", "GGZ-007", "MRT-628", "YGA-441"]
+    states = ["Alaska", "Colorado", "Hawaii", "Louisiana"]
+    fines = [25, 50, 75 ,100]
 
-    for i in range(len(boats)):
-        problem.addVariable("boats" + str(i), boats)
-        problem.addVariable("captains" + str(i), captains)
-        problem.addVariable("locations" + str(i), locations)
-        problem.addVariable("manatees" + str(i), manatees)
+    for i in range(len(cars)):
+        problem.addVariable("cars" + str(i), cars)
+        problem.addVariable("license_plates" + str(i), license_plates)
+        problem.addVariable("states" + str(i), states)
+        problem.addVariable("fines" + str(i), fines)
 
 
     problem.addConstraint(AllDifferentConstraint())
-    #boats is now the identifier
-    problem.addConstraint(lambda a, b, c, d: a == "Daily Ray" and b == "Foxy Roxy" and c == "Samantha" and d == "Watery Pete", ("boats0", "boats1", "boats2", "boats3"))
+    #cars is now the identifier
+    problem.addConstraint(lambda a, b, c, d: a == "Cavalo" and b == "Fierro" and c == "Grandero" and d == "Injitsu", ("cars0", "cars1", "cars2", "cars3"))
 
-    #FIXME: change for new problem all below
-    for i in range(len(boats)):
-        problem.addConstraint(FunctionConstraint(hint1), ["boats" + str(i), "locations" + str(i), "captains" + str(i)])
-        problem.addConstraint(FunctionConstraint(hint2), ["boats" + str(i), "locations" + str(i), "manatees" + str(i), "manatees3"])
-        problem.addConstraint(FunctionConstraint(hint3), ["boats" + str(i), "locations" + str(i), "captains" + str(i)])
-        for j in range(len(boats)):
-            problem.addConstraint(FunctionConstraint(hint4), ["locations" + str(i), "locations" + str(j), "manatees" + str(i), "manatees" + str(j)])
-        problem.addConstraint(FunctionConstraint(hint5), ["locations" + str(i), "manatees" + str(i)])
-        problem.addConstraint(FunctionConstraint(hint6), ["boats" + str(i), "manatees" + str(i), "captains" + str(i)])
-        #We know foxy roxy sub set ends in 1
-        problem.addConstraint(FunctionConstraint(hint7), ["boats" + str(i), "locations" + str(i), "manatees" + str(i), "captains" + str(i), "boats1", "manatees1", "captains1"])
-        problem.addConstraint(FunctionConstraint(hint8), ["boats" + str(i), "locations" + str(i)])
+    #change for new problem all below
+    for i in range(len(cars)):
+        problem.addConstraint(FunctionConstraint(hint1), ["cars" + str(i), "states" + str(i), "license_plates" + str(i)])
+        problem.addConstraint(FunctionConstraint(hint2), ["cars" + str(i), "states" + str(i), "fines" + str(i), "fines3"])
+        problem.addConstraint(FunctionConstraint(hint3), ["cars" + str(i), "states" + str(i), "license_plates" + str(i)])
+        for j in range(len(cars)):
+            problem.addConstraint(FunctionConstraint(hint4), ["states" + str(i), "states" + str(j), "fines" + str(i), "fines" + str(j)])
+        problem.addConstraint(FunctionConstraint(hint5), ["states" + str(i), "fines" + str(i)])
+        problem.addConstraint(FunctionConstraint(hint6), ["cars" + str(i), "fines" + str(i), "license_plates" + str(i)])
+        #We know Fierro subset ends in 1
+        problem.addConstraint(FunctionConstraint(hint7), ["cars" + str(i), "states" + str(i), "fines" + str(i), "license_plates" + str(i), "cars1", "fines1", "license_plates1"])
 
     print "Number of Solutions: ", len(problem.getSolutions())
     for answer in problem.getSolutions():
